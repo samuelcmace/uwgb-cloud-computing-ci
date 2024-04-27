7ea98ff36556085ea4cff615037b435aaf9d8176
Samuel Mace <samuelcmace@gmail.com>
2024-04-27 18:41:50 -0500

    Update Terraform to Utilize Load Balancer

--- website/main.tf
+++ website/main.tf
@@ -22,18 +22,6 @@ resource "aws_s3_bucket" "cs293ci" {
     }
 }
 
-resource "aws_s3_bucket_website_configuration" "cs293ci_website_configuration" {
-    bucket = aws_s3_bucket.cs293ci.id
-
-    index_document {
-      suffix = "index.html"
-    }
-
-    error_document {
-      key = "error.html"
-    }
-}
-
 data "aws_iam_policy_document" "cs293ci_iam_policy_document" {
     statement {
         principals {
@@ -42,7 +30,7 @@ data "aws_iam_policy_document" "cs293ci_iam_policy_document" {
         }
         sid = "PublicReadGetObject"
         actions = ["s3:GetObject"]
-        resources = ["arn:aws:s3:::${aws_s3_bucket.cs293ci.bucket}/*"]
+        resources = ["${aws_s3_bucket.cs293ci.arn}/*"]
         effect = "Allow"
     }
 }
@@ -50,4 +38,51 @@ data "aws_iam_policy_document" "cs293ci_iam_policy_document" {
 resource "aws_s3_bucket_policy" "cs293ci_bucket_policy" {
     bucket = aws_s3_bucket.cs293ci.id
     policy = data.aws_iam_policy_document.cs293ci_iam_policy_document.json
+}
+
+resource "aws_cloudfront_origin_access_control" "cs293ci_cloudfront_origin_access_control" {
+    name = "cs293ci_cloudfront_origin_access_control"
+    description = "Cloudfront Origin Access Control for the cs293ci Bucket"
+    origin_access_control_origin_type = "s3"
+    signing_behavior = "always"
+    signing_protocol = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cs293ci_cloudfront_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.cs293ci.bucket_regional_domain_name
+        origin_access_control_id = aws_cloudfront_origin_access_control.cs293ci_cloudfront_origin_access_control.id
+        origin_id = aws_s3_bucket.cs293ci.id
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = aws_s3_bucket.cs293ci.id
+
+        forwarded_values {
+          query_string = false
+
+          cookies {
+            forward = "none"
+          }
+        }
+
+        viewer_protocol_policy = "allow-all"
+    }
+
+    restrictions {
+      geo_restriction {
+        restriction_type = "whitelist"
+        locations = ["US", "CA", "GB", "DE"]
+      }
+    }
+
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
 }
